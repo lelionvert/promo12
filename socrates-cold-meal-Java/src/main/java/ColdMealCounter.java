836756700e08f0cbe992f23889a4af56dfe239bb
Amadou <amadousy@outlook.com>
2020-01-15 18:13:29 +0100

    refactor isColdMeal method

--- socrates-cold-meal-Java/src/main/java/ColdMealCounter.java
+++ socrates-cold-meal-Java/src/main/java/ColdMealCounter.java
@@ -6,19 +6,11 @@ public class ColdMealCounter {
     public boolean isColdMeal(LocalDateTime date) {
 
         LocalDateTime ninePm = LocalDateTime.of(2020, 10, 29, 21, 00);
+        LocalDateTime oneAm = LocalDateTime.of(2020, 10, 30, 1, 00);
 
-        if(date.isBefore(ninePm)){
+        if(date.isBefore(ninePm) || date.isEqual(ninePm) || date.isAfter(oneAm)){
             return false;
         }
-
-        if(date.isAfter(LocalDateTime.of(2020, 10, 30, 1, 00))){
-            return false;
-        }
-
-        if(date.isEqual(ninePm)){
-            return false;
-        }
-
 
         return true;
     }
