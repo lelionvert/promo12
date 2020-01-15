0d73ed9eb5970851d837ba6bda22a4344620c1cd
Inferno47 <aymericgand@lacombedulionvert.fr>
2020-01-15 16:54:43 +0100

    Test limit and no hour

--- csharp/SoCraTesFrOrganizer/MealsCountColdMealsTest.cs
+++ csharp/SoCraTesFrOrganizer/MealsCountColdMealsTest.cs
@@ -118,5 +118,43 @@ namespace SoCraTesFrOrganizer
 
             Assert.AreEqual(1, nb);
         }
+
+        [Test]
+        public void CountColdMealAt21Hours()
+        {
+            List<DateTime> l_Datetime = new List<DateTime>()
+            {
+                new DateTime(2020, 10, 29, 21, 00, 00),
+            };
+            MealsCountColdMeals l_MealsCountColdMeals = new MealsCountColdMeals(l_Datetime);
+            var nb = l_MealsCountColdMeals.count();
+
+            Assert.AreEqual(0, nb);
+        }
+
+        [Test]
+        public void CountColdMealAt1Hours()
+        {
+            List<DateTime> l_Datetime = new List<DateTime>()
+            {
+                new DateTime(2020, 10, 30, 1, 00, 00),
+            };
+            MealsCountColdMeals l_MealsCountColdMeals = new MealsCountColdMeals(l_Datetime);
+            var nb = l_MealsCountColdMeals.count();
+
+            Assert.AreEqual(1, nb);
+        }
+
+        [Test]
+        public void CountColdMealNohours()
+        {
+            List<DateTime> l_Datetime = new List<DateTime>()
+            {
+            };
+            MealsCountColdMeals l_MealsCountColdMeals = new MealsCountColdMeals(l_Datetime);
+            var nb = l_MealsCountColdMeals.count();
+
+            Assert.AreEqual(0, nb);
+        }
     }
 }
