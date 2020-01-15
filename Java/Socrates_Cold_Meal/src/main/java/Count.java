f0f232a273d375e2a4ba0946e7e0ee50166799b2
Life Light <lifelightdev@gmail.com>
2020-01-16 00:44:45 +0100

    Implement FindCheckInAfter21HWihtDateTest

--- Java/Socrates_Cold_Meal/src/main/java/Count.java
+++ Java/Socrates_Cold_Meal/src/main/java/Count.java
@@ -1,7 +1,10 @@
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Count {
     public static boolean FindCheckInAfter21H(LocalDateTime ldt) {
-        return true;
+        LocalTime lt21H = LocalTime.of(21,0);
+        LocalTime lt = LocalTime.of(ldt.getHour(),ldt.getMinute());
+        return lt.isAfter(lt21H);
     }
 }
