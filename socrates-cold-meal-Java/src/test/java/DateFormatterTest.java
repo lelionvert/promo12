b4d5c9a5ee293139492506432d0efb21d4f5dcc3
Amadou <amadousy@outlook.com>
2020-01-16 10:26:44 +0100

    Convert text to date

--- socrates-cold-meal-Java/src/test/java/DateFormatterTest.java
+++ socrates-cold-meal-Java/src/test/java/DateFormatterTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +14,30 @@ class DateFormatterTest {
         assertThat(dateFormatter.textToDate("29/10/2020 19:32", "dd/MM/yyyy HH:mm"))
                 .isEqualTo(LocalDateTime.of(2020, 10, 29, 19, 32));
     }
+
+    @Test
+    public void invalidDate(){
+        DateFormatter dateFormatter = new DateFormatter();
+        Assertions.assertThrows(DateTimeParseException.class, () -> {
+            dateFormatter.textToDate("32/10/2020 19:32", "dd/MM/yyyy HH:mm");
+        });
+    }
+
+    @Test
+    public void emptyDate(){
+        DateFormatter dateFormatter = new DateFormatter();
+        Assertions.assertThrows(DateTimeParseException.class, () -> {
+            dateFormatter.textToDate("", "dd/MM/yyyy HH:mm");
+        });
+    }
+
+    @Test
+    public void notADate(){
+        DateFormatter dateFormatter = new DateFormatter();
+        Assertions.assertThrows(DateTimeParseException.class, () -> {
+            dateFormatter.textToDate("NOT A DATE", "dd/MM/yyyy HH:mm");
+        });
+    }
+
 
 }
