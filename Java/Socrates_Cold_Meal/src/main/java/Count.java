d5f8bffefa231917e894ce075735d3ba38185a2d
Life Light <lifelightdev@gmail.com>
2020-01-16 01:16:10 +0100

    impl FindChecInForColdMeal

--- Java/Socrates_Cold_Meal/src/main/java/Count.java
+++ Java/Socrates_Cold_Meal/src/main/java/Count.java
@@ -3,14 +3,16 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Count {
+
+    static LocalTime lt21H = LocalTime.of(21,0);
+    static LocalTime lt1H = LocalTime.of(1,0);
+
     public static boolean FindCheckInAfter21H(LocalDateTime ldt) {
-        LocalTime lt21H = LocalTime.of(21,0);
         LocalTime lt = LocalTime.of(ldt.getHour(),ldt.getMinute());
         return lt.isAfter(lt21H);
     }
 
     public static boolean FindChecInkBefore1H(LocalDateTime ldt) {
-        LocalTime lt1H = LocalTime.of(1,0);
         LocalTime lt = LocalTime.of(ldt.getHour(),ldt.getMinute());
         return lt.isBefore(lt1H);
     }
@@ -24,6 +26,6 @@ public class Count {
     }
 
     public static boolean FindChecInForColdMeal(LocalDateTime ldt) {
-        return true;
+        return FindCheckInAfter21HInThursday(ldt) && FindChecInkBefore1HInFriday(ldt);
     }
 }
